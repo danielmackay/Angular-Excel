73af8cb84f9edb3ca8b0180f4743a799c6e2536d
Daniel Mackay <crankage@gmail.com>
2014-12-13 18:55:36 +1100

    Initial commit.  Basic functionality working via controller.

---
+++ Angular Excel/app/app.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', []);
+
+    app.controller('appCtrl', appCtrl);
+
+    function appCtrl() {
+        var self = this;
+
+        self.attributes = [
+            {
+                id: 1,
+                value: 1
+            },
+            {
+                id: 2,
+                value: 2
+            }
+        ];
+
+        self.attributeTypes = [
+            {
+                name: 'red',
+                id: 1
+            },
+            {
+                name: 'blue',
+                id: 2
+            },
+            {
+                name: 'green',
+                id: 3
+            }
+        ];
+
+        self.addAttribute = function() {
+            self.attributes.push({});
+        }
+
+        self.save = function() {
+            alert('saved');
+        }
+    }
+})();
